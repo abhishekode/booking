4db04d49684490006698e99be1d54a151880f0ed
Abhishek <abhishek.pandey@inaraconsultancy.com>
2024-02-16 17:56:11 +0530

    update bg color

--- src/pages/Booking/BookingTab.tsx
+++ src/pages/Booking/BookingTab.tsx
@@ -12,7 +12,10 @@ const BookingTab: React.FC<BookingTabProps> = ({ seat, bookedSeats, selectedSeat
         <button
             key={seat}
             disabled={bookedSeats.includes(seat)}
-            className={`${bookedSeats.includes(seat) ? 'bg-[#ff6961]' : 'bg-gray-200'} ${selectedSeat === seat ? 'bg-gray-900 text-gray-50' : 'bg-gray-200'} px-2 py-3`}
+            className={`
+            ${bookedSeats.includes(seat) ? 'bg-red-600' : 'bg-gray-200'} 
+            ${selectedSeat === seat ? 'bg-gray-900 text-gray-50' : 'bg-gray-200'} 
+            px-2 py-3`}
             onClick={() => onClickBook(seat)}
         >
             {seat.substring(5)}
